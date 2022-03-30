369870a9e2ed1fc28dded48dd1c369ea8e5f8cf9
tienshake <tientrancntt@gmail.com>
2022-03-30 11:02:54 +0700

    fisrt-commit

---
+++ src/components/admin/Admin.js
@@ -0,0 +1,97 @@
+import {
+    Link,
+    Outlet,
+} from 'react-router-dom';
+import {
+    FaRegBell,
+    FaUsers
+} from "react-icons/fa";
+import './Admin.scss';
+import { IoAppsOutline, IoPhonePortraitOutline, IoCreate } from "react-icons/io5";
+import { AiFillMail, AiOutlineUser, AiOutlineFileSync, AiOutlineUpload } from "react-icons/ai";
+import UserManage from './UserMange';
+import React, { useState, useEffect } from 'react';
+import { createUserService, getAllUCodeService, createProductService } from '../../services/userService';
+import { alert } from 'react-bootstrap-confirmation';
+import { confirm } from 'react-bootstrap-confirmation';
+
+const Admin = (props) => {
+    const createToAdmin = async (data, type) => {
+        try {
+            const isConfirm = await confirm("Bạn có muốn tạo không?");
+
+            if (isConfirm) {
+                let res = '';
+                if (type === "CREATE-USER") {
+                    res = await createUserService(data);
+                }
+                if (type === "CREATE-PRODUCT") {
+                    res = await createProductService(data);
+                }
+                if (res && res.errCode === 0) {
+                    alert("Bạn đã tạo thành công");
+                    console.log(res);
+                } else {
+                    alert("Bạn đã thất bại")
+                }
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
+    return (
+        <div className="manage__container">
+            <div className="manage__nav">
+                <div className="dashboard__header">Dashboard</div>
+                <ul className='manage__nav-list'>
+                    <h4>Người dùng</h4>
+                    <Link className="link" to="create-user"><li> <AiOutlineUser /><span>Tạo người dùng</span></li></Link>
+                    <Link className="link" to="users"><li> <FaUsers /><span>Danh sách người dùng</span></li></Link>
+                    <h4>Sản phẩm</h4>
+                    <Link className="link" to="create-product"><li> <IoCreate /><span>Tạo sản phẩm</span></li></Link>
+                    <li><IoCreate /><span>Chi tiết sản phẩm</span></li>
+                    <li><IoPhonePortraitOutline /><span>Sản phẩm</span></li>
+                    <li><IoPhonePortraitOutline /><span>khuyến mãi tặng kèm</span></li>
+                    <li><AiOutlineFileSync /><span>Đơn hàng</span></li>
+                </ul>
+            </div>
+            <div className="manage__body">
+                <div className="manage__header">
+                    <div className="manage__header-top">
+                        <ul className="manage__header-top-left">
+                            <li><Link className="link" to="/">Trang chủ</Link></li>
+                            <li>User</li>
+                            <li>Settings</li>
+                        </ul>
+                        <ul className="manage__header-top-right">
+                            <li> <FaRegBell /></li>
+                            <li><IoAppsOutline /></li>
+                            <li><AiFillMail /></li>
+                            <li>
+                                <div className="avatar"
+                                    style={{
+                                        backgroundImage: "url(" + "https://anhdep123.com/wp-content/uploads/2021/02/anh-avatar-hai-huoc.jpg" + ")",
+                                        backgroundPosition: 'center',
+                                        backgroundSize: 'cover',
+                                        backgroundRepeat: 'no-repeat'
+                                    }}
+                                ></div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="manage__header-bottom">
+                    </div>
+
+                </div>
+                <div className="manage__content">
+                    <div className="manage__content-center">
+                        <Outlet context={{ createToAdmin }} />
+                    </div>
+                </div>
+            </div>
+
+        </div >
+    );
+}
+
+export default Admin;
---
+++ src/services/userService.js
@@ -0,0 +1,22 @@
+import axios from '../axios';
+
+
+const getAllUCodeService = async (id) => {
+    return await axios.get(`/api/AllCode?id=${id}`);
+}
+const getAllUserService = async (id, limit, page) => {
+    return await axios.get(`/api/getAllUser?id=${id}&limit=${limit}&page=${page}`);
+}
+const createUserService = async (data) => {
+    return await axios.post('/api/createUser', data);
+};
+const createProductService = async (data) => {
+    return await axios.post('/api/create-product', data);
+};
+
+export {
+    createUserService,
+    getAllUserService,
+    getAllUCodeService,
+    createProductService
+}
